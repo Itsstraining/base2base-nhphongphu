03f9a2cb8f1cbfe3513261cb2f7b9e34618ab3c3
Phong Phú <phongphu.official@gmail.com>
2021-01-31 16:33:06 +0700

    test

--- program.js
+++ program.js
@@ -1,8 +1,25 @@
-// Add new functions, variables here
+function decimalToBinary(convertNumber) {
+  let binary = 0;
+  let remainder, i = 1;
+  while (convertNumber != 0) {
+    remainder = convertNumber % 2;
+    convertNumber = parseInt(convertNumber / 2);
+    binary = binary + remainder * i;
+    i = i * 10;
+  }
+  return binary;
+}
 
 function main(input) {
-  // Your code goes here
-  // Using console.log to answer the question
+  let arrOutput = input.split(' ');
+  let convertNumber = parseInt(arrOutput[0]);
+  let convertFrom = parseInt(arrOutput[1]);
+  let convertTo = parseInt(arrOutput[2]);
+  decimalToBinary(convertNumber);
+
+  if (arrOutput[1] == 10 && arrOutput[2] == 2) {
+    console.log(decimalToBinary(convertNumber))
+  }
 }
 
 module.exports = main;
